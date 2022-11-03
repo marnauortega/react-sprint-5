0fae95e5b42ab690d3189ec2b6e40defcb9d8674
Martí Arnau <marnauortega@gmail.com>
2022-11-03 11:52:57 +0100

    n1 - exercice 2

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,13 +1,22 @@
-async function fetcher(url = "", headers = {}) {
+async function fetcher(url = "", headers = {}, callback) {
   const response = await fetch(url, headers);
   const data = await response.json();
-  console.log(data);
+  console.log(data.joke);
+  callback(data.joke);
+}
+
+const url = "https://icanhazdadjoke.com/";
+const headers = {
+  headers: {
+    Accept: "application/json",
+  },
+};
+const jokeParagraph = document.querySelector(".joke");
+
+function insertJoke(joke) {
+  jokeParagraph!.innerHTML = joke;
 }
 
 document.querySelector(".next-joke")?.addEventListener("click", () => {
-  fetcher("https://icanhazdadjoke.com/", {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  fetcher(url, headers, insertJoke);
 });
